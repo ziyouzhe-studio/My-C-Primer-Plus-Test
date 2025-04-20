c54524bd07a23604c63daadf7ea18f002dfc53dd
ziyouzhe <wuwei20015291@outlook.com>
2025-04-20 21:00:14 +0800

    添加了一些新的转换说明方法

--- code/04/4.8_floats.c
+++ code/04/4.8_floats.c
@@ -5,4 +5,11 @@ int main(void)
     const double RENT = 3852.99; // const变量
     printf("%f*\n", RENT);
     printf("*%e*\n", RENT);
+    printf("*%4.2f*\n", RENT); // 4.2f表示总宽度为4，精度为2
+    printf("*%3.1f*\n", RENT); // 3.1f表示总宽度为3，精度为1
+    printf("*%10.3f*\n", RENT); // 10.3f表示总宽度为10，精度为3
+    printf("*%10.3E*\n", RENT); // 10.3E表示总宽度为10，精度为3
+    printf("*%+4.2f*\n", RENT); // +4.2f表示总宽度为4，精度为2，带符号
+    printf("*%010.2f*\n", RENT); // 010.2f表示总宽度为10，精度为2，前面补0
+    return 0;
 }
